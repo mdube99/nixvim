b2f4a2f43a52a5fb44412a1fa229c32ad75d4de5
mdube99 <mjdube99@gmail.com>
2024-05-14 21:48:12 +0000

    changed cmp to tab in normal mode

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -35,11 +35,9 @@
           completion = {border = "solid";};
           documentation = {border = "solid";};
         };
-
         mapping = {
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<Tab>" = "cmp.mapping.select_next_item()";
+          "<S-Tab>" = "cmp.mapping.select_prev_item()";
           "<C-e>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
